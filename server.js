9b6829d0f46200162f336e1e160c3bd143243b9b
tales-net <tales.net2019@gmail.com>
2026-08-05 02:57:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const crypto = require('crypto');
-const { RouterOSClient } = require('routeros-client');
+const { RouterOSClient } = require('node-routeros');
 
 // ======================================================
 // ⚙️ جميع البيانات الخاصة بك مدمجة وجاهزة
